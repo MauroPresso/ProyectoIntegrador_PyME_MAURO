82b3a6ff97e31ac6d10bbb29c128e2deb0bf3b02
Mauro Presso <mauro.presso@gmail.com>
2026-06-03 14:44:48 -0300

    Agregar validacion de creacion de base de datos

---
+++ 10_Pruebas_Optimizacion/Validaciones/01_verificar_creacion_base_datos.sql
@@ -0,0 +1,28 @@
+﻿/* ============================================================
+   PROYECTO INTEGRADOR BDD
+   VALIDACION DE CREACION DE BASE DE DATOS
+
+   Base de datos: BaseDeDatos_PyME
+   Sirve para verificar que la base fue creada correctamente
+   y que los archivos fisicos .mdf y .ldf estan en la ruta esperada.
+   ============================================================ */
+
+USE BaseDeDatos_PyME;
+GO
+
+SELECT DB_NAME() AS base_actual;
+GO
+
+SELECT 
+    name AS nombre_logico,
+    physical_name AS ruta_fisica
+FROM sys.database_files;
+GO
+
+SELECT 
+    name,
+    database_id,
+    create_date
+FROM sys.databases
+WHERE name = 'BaseDeDatos_PyME';
+GO
